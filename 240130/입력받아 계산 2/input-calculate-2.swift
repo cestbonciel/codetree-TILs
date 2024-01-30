c30f24a004efab2c566b2f77481d37a29ccc7c3f
Kim Seo hyun <46340978+cestbonciel@users.noreply.github.com>
2024-01-30 15:17:23 +0900

    Title: 입력받아 계산 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240130/입력받아 계산 2/input-calculate-2.swift
+++ 240130/입력받아 계산 2/input-calculate-2.swift
@@ -1,4 +1,4 @@
 func executeMultiple(a: Int, b: Int) {
-    guard a >= 1, a <= 100 && b >= 1, b <= 100 else { return }
-    print(a * b, separator: " ")
+    guard a >= 1, a <= 100, b >= 1, b <= 100 else { return }
+    print(a * b)
 }
