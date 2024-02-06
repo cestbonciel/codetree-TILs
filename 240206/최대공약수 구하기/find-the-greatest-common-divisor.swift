c89af516e3ad6fe39e288408107b763471c5929d
Kim Seo hyun <46340978+cestbonciel@users.noreply.github.com>
2024-02-06 19:48:38 +0900

    Title: 최대공약수 구하기, Time: 28ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/최대공약수 구하기/find-the-greatest-common-divisor.swift
+++ 240206/최대공약수 구하기/find-the-greatest-common-divisor.swift
@@ -17,9 +17,7 @@ func gcdIterativeEuklid(_ m: Int, _ n: Int) {
 if let input = readLine() {
     let values = input.split(separator: " ").compactMap { Int($0) }
     guard values.count == 2,
-          let a = values.first, let b = values.last,
-          (1...100).contains(a),
-          (1...100).contains(b) else {
+          let a = values.first, let b = values.last else {
         fatalError("유효하지 않은 입력이거나 범위를 벗어났습니다.")
     }
     gcdIterativeEuklid(a, b)
